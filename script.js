381931569b4cb83e9e044e7a231ffce60dfeb78d
ConnorRuth <152222829+ConnorRuth@users.noreply.github.com>
2024-01-13 12:23:19 -0800

    radio buttons invisible & label colors

--- script.js
+++ script.js
@@ -1,8 +1,13 @@
 var ans1El = document.querySelector("#answer1Text");
+var btn1El = document.querySelector("#answer1");
 var ans2El = document.querySelector("#answer2Text");
+var btn2El = document.querySelector("#answer2");
 var ans3El = document.querySelector("#answer3Text");
+var btn3El = document.querySelector("#answer3");
 var ans4El = document.querySelector("#answer4Text");
+var btn4El = document.querySelector("#answer4");
 var questionEl = document.querySelector("#question");
+var submitEl = document.querySelector("#submit");
 var answered = 0;
 var questionArr = [
     {
@@ -24,15 +29,33 @@ var questionArr = [
 
 ]
 
+function getResponse(event){
+    event.preventDefault();
+    var response = ""; 
+    if(btn1El.checked === true){
+        response = ans1El.textContent;
+    }else if(btn2El.checked === true){
+        response = ans2El.textContent;
+    }
+   
+   console.log(response)
+}
+
+
+
 if (answered < questionArr.length ) {
     let i = answered;
     questionEl.textContent = questionArr[i].ask;
     ans1El.textContent = questionArr[i].guestAns1;
+    ans1El.value = questionArr[i].guestAns1;
     ans2El.textContent = questionArr[i].guestAns2;
+    ans2El.value = questionArr[i].guestAns2;
     ans3El.textContent = questionArr[i].guestAns3;
+    ans3El.value = questionArr[i].guestAns3;
     ans4El.textContent = questionArr[i].guestAns4;
+    ans4El.value = questionArr[i].guestAns4;
 }
 
-
+submitEl.addEventListener("click", getResponse);
 
 console.log(ans1El.textContent);
