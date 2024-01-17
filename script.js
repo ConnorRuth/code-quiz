db074417c6f5db711b06809a8306142516c4a029
ConnorRuth <152222829+ConnorRuth@users.noreply.github.com>
2024-01-17 13:47:45 -0800

    Added hide and show functionality

--- script.js
+++ script.js
@@ -1,15 +1,14 @@
 var ans1El = document.querySelector("#answer1Text");
-var btn1El = document.querySelector("#answer1");
 var ans2El = document.querySelector("#answer2Text");
-var btn2El = document.querySelector("#answer2");
 var ans3El = document.querySelector("#answer3Text");
-var btn3El = document.querySelector("#answer3");
 var ans4El = document.querySelector("#answer4Text");
-var btn4El = document.querySelector("#answer4");
 var questionEl = document.querySelector("#question");
 var submitEl = document.querySelector("#submit");
 var timeEl = document.getElementById('time');
-var startQuizEl =document.getElementById('start');
+var startQuizEl = document.getElementById('start');
+var submitQuizEl = document.getElementById('giveName');
+var scoreListEl = document.getElementById('highscores');
+var scoreLinkEl = document.getElementById('scoreLink');
 var answered = 0;
 var timeLeft = 100;
 var questionArr = [
@@ -33,12 +32,17 @@ var questionArr = [
 function hideSectionEl() {
     var startEl = document.getElementById("entry");
     var quizEl = document.getElementById("theQuiz");
-    startEl.style.display = "none";
-    quizEl.style.display = "contents";
+    startEl.classList.add("makeHidden");
+    quizEl.classList.remove("makeHidden");
+
 }
 // check which button is checked and set response to it when response is equal to array.answer they answered correctly
 function getResponse(event){
     event.preventDefault();
+    var btn1El = document.querySelector("#answer1");
+    var btn2El = document.querySelector("#answer2");
+    var btn3El = document.querySelector("#answer3");
+    var btn4El = document.querySelector("#answer4");
     var response = ""; 
     let i = answered
     if(btn1El.checked){
@@ -61,6 +65,7 @@ function getResponse(event){
 //changes the questions and answers to go down the options in the array
 function changeQuestion(){
 if (answered < questionArr.length ) {
+    
     let i = answered;
     questionEl.textContent = questionArr[i].ask;
     ans1El.textContent = questionArr[i].guestAns1;
@@ -77,8 +82,10 @@ function countdown() {
     var timerInterval = setInterval(function() {
             timeLeft--;
             timeEl.textContent = "Time: " + timeLeft;
-        if(timeLeft <= 0 || answered > questionArr.length){
+        if(timeLeft <= 0 || answered === questionArr.length){
             var score = timeLeft ;
+            document.getElementById("theQuiz").classList.add("makeHidden");
+            document.getElementById("scorePrompt").classList.remove("makeHidden");
             if(score < 0){
                 score = 0;
             }
@@ -88,9 +95,20 @@ function countdown() {
     }, 500);
 }
 function showScores(){
-
+    if(scoreListEl.classList.contains("makeHidden")){
+        scoreListEl.classList.remove("makeHidden");
+    } else scoreListEl.classList.add("makeHidden");
 }
+function submitScore(event) {
+    event.preventDefault();
+    document.getElementById("scorePrompt").classList.add("makeHidden");
+    scoreListEl.classList.remove("makeHidden");
+}
+changeQuestion();
  startQuizEl.addEventListener('click', countdown);
  startQuizEl.addEventListener('click', hideSectionEl);
  submitEl.addEventListener("click", getResponse);
-changeQuestion();
+ submitQuizEl.addEventListener('click', submitScore);
+ console.log(typeof scoreListEl.classList.contains("makeHidden"));
+ scoreLinkEl.addEventListener("click", showScores);
+
